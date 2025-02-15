4720d4f8e601639776db87d9c0973e25ceba38fc
Aleksey Khlopkov <v.v.sidorovgg@gmail.com>
2025-02-15 19:04:03 +0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    
+
+    @Get() // GET /users or /users?role=value&age=42
+    findAll(@Query("role") role?: 'INTERN' | 'INGENEER' | 'ADMIN') {
+        return [];
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param("id") id: string) {
+        return {id}
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        return user;
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param("id") id: string, @Body() userUpdate: {}) {
+        return {id, ...userUpdate};
+    }
+
+    @Delete(':id') // GET DELETE /users/:id
+    delete(@Param("id") id: string) {
+        return {id}
+    }
+}
